9ddbc44daeceedccc5b535e6a72db8dc705e9f20
jinha <gammazx@naver.com>
2024-04-29 10:59:29 +0900

    [feature] 31

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -23,4 +23,11 @@ class GameTest {
             game.guess(null);
         });
     }
+
+    @Test
+    void throwExceptionWhenInputLengthIsUnmathced() {
+        assertThrows(IllegalArgumentException.class, ()-> {
+            game.guess("12");
+        });
+    }
 }
