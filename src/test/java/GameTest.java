be4177f19494fd12ed5dedb47015651d5e35f026
jinha <gammazx@naver.com>
2024-04-29 12:27:44 +0900

    [feature] 52

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
@@ -33,5 +34,16 @@ class GameTest {
         } catch (IllegalArgumentException e) {
 
         }
+    }
+
+    @Test
+    void returnSolvedResultIfMatchedNumber() {
+        game.question = "123";
+        GuessResult result = game.guess("123");
+
+        assertThat(result).isNotNull();
+        assertThat(result.solved).isEqualTo(true);
+        assertThat(result.strikes).isEqualTo(3);
+        assertThat(result.balls).isEqualTo(0);
     }
 }
